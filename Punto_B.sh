0f88567e90c8787b3a0762e1854d5b8321e195bb
NahuelUS <nahuel.uchu@gmail.com>
2025-07-03 05:45:27 +0000

    Agrego comandos del punto B

---
+++ Punto_B.sh
@@ -0,0 +1,7 @@
+cd ~/RTA_Examen_20250703
+sudo mkdir -p /usr/local/bin/Uchupe
+vim AltaUser-Groups.sh
+chmod +x AltaUser-Groups.sh
+sudo cp AltaUser-Groups.sh /usr/local/bin/Uchupe/AltaUser-Groups.sh
+/usr/local/bin/Uchupe/AltaUser-Groups.sh vagrant ../UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt
+
